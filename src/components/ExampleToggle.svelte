bc9fe94c46e2977c2039b81135cb6846b8a42615
Kevin Gawlik <kevin@gawlik.io>
2020-06-23 18:53:30 +0200

    improve ui

--- src/components/ExampleToggle.svelte
+++ src/components/ExampleToggle.svelte
@@ -12,11 +12,14 @@
             ml-4
             text-2xl
             text-gray-900
+            flex
+            items-center
             hover:text-gray-700
             dark:text-gray-50
             dark:hover:text-gray-200
         ">
-            Transtion with toggle
+            <svg class="h-6 w-6 mr-2" fill="none" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" viewBox="0 0 24 24" stroke="currentColor"><path d="M13.828 10.172a4 4 0 00-5.656 0l-4 4a4 4 0 105.656 5.656l1.102-1.101m-.758-4.899a4 4 0 005.656 0l4-4a4 4 0 00-5.656-5.656l-1.1 1.1"></path></svg>
+            Transition with toggle
         </h2>
     </a>
     <label class="
